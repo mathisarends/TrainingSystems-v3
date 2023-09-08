b8b2dc2b2853fd7f464bb07bd32c2d7f75c17065
mathisarends <140075923+mathisarends@users.noreply.github.com>
2023-09-08 08:29:09 +0200

    service worker ohne nots

--- public/javascripts/timer-service-worker.js
+++ public/javascripts/timer-service-worker.js
@@ -38,18 +38,14 @@ self.addEventListener('message', function(event) {
         // Hier können Sie die Nachricht senden, wenn der Timer abgelaufen ist
         // Selbst wenn der Bildschirm gesperrt ist oder das Handy im Energiesparmodus ist
         self.registration.showNotification('Timer', {
-          title: "Timer",
           body: 'Ihr Timer ist abgelaufen!',
-          tag: 'timer-notification', // Eindeutiger Tag für die Benachrichtigung
-          requireInteraction: true,
-          vibrate: [200, 100, 200] // Eine Vibration von 200 ms, eine Pause von 100 ms und dann wieder 200 ms Vibration
      
         });
       } else {
         remainingTime -= interval;
         //for push notification: 
                 //for push notification: 
-                const remainingTimeInSeconds = remainingTime / 1000;
+/*                 const remainingTimeInSeconds = remainingTime / 1000;
                 const minutes = Math.floor(remainingTimeInSeconds / 60);
                 const seconds = Math.floor(remainingTimeInSeconds % 60);
                 const formattedTime = `${String(minutes).padStart(2, "0")}:${String(
@@ -62,7 +58,7 @@ self.addEventListener('message', function(event) {
                   title: "Timer",
                   body: `Verbleibende Zeit: ${formattedTime} Sekunden`,
                   tag: 'timer-notification', // Eindeutiger Tag für die Benachrichtigung
-                });
+                }); */
   
         
         // Senden Sie die verbleibende Zeit in jeder Iteration an das Frontend
