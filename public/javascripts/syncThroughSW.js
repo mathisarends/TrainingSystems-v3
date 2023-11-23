cc52bb8f5f2a7ad6d0af11fb05f4944a88144f1f
mathisarends <140075923+mathisarends@users.noreply.github.com>
2023-11-23 20:22:41 +0100

    refactored syncThroughSW.js

--- public/javascripts/syncThroughSW.js
+++ public/javascripts/syncThroughSW.js
@@ -1,82 +1,137 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // check whether there is a service worker already installed
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.getRegistration().then((registration) => {
-      if (registration && registration.active) {
-        handleNetworkModeSelector(registration);
+const offlineModal = document.getElementById("offlineModal");
+
+// svgs for user feedback
+const onlineSVG = document.getElementById("online-svg");
+const offlineSVG = document.getElementById("offline-svg");
+const syncSVG = document.getElementById("sync-svg");
+
+const DESKTOP_MIN_WIDTH = 1024;
+
+const showModal = () => {
+  const offlineModal = document.getElementById("offlineModal");
+  offlineModal.style.display = "block";
+}
+
+const hideModal = () => {
+  const offlineModal = document.getElementById("offlineModal");
+  offlineModal.style.display = "none";
+  offlineModal.querySelector("input").value = "Offline Daten synchronsiert";
+}
+
+const showOnlineSVG = () => {
+  onlineSVG.style.display = "block";
+  syncSVG.style.display = "none";
+  offlineSVG.style.display = "none";
+}
+
+const showOfflineSVG = () => {
+  onlineSVG.style.display = "none";
+  offlineSVG.style.display = "block";
+}
+
+const showSyncButton = () => {
+  syncSVG.style.display = "block";
+  onlineSVG.style.display = "none";
+}
+
+
+const handleStatusBarClick = (showStatusBar, statusBar) => {
+  showStatusBar.style.display = "none";
+  statusBar.style.transform = "translateY(0%)";
+}
+
+const handleCloseModalButton = (waitForSyncModal) => {
+  e.preventDefault();
+  waitForSyncModal.style.display = "none";
+  waitForSyncModal.classList.remove("active");
+}
+
+const switchToOfflineMode = (registration) => {
+  registration.active.postMessage("switchToOfflineMode");
+  syncSVG.style.display = "none";
+  localStorage.setItem("wifi", false);
+  showOfflineSVG(); //switch view to offline - always display current status
+}
+
+const switchToDefaultMode = (registration, registratedUserID) => {
+  registration.active.postMessage({
+    command: "switchToDefaultMode",
+    registratedUser: registratedUserID,
+  });
+  localStorage.setItem("wifi", true);
+  showOnlineSVG();
+}
+
+const tryToSwitchToDefaultMode = (registration, registratedUserID) => {
+  registration.active.postMessage({command: "getOnlineStatus"}); //get actual online status of serviceWorker
+
+  navigator.serviceWorker.addEventListener("message", (event) => { //react to online status
+    const data = event.data;
+
+    if (data.type === "swOnlineStatus") {
+      if (data.onlineStatus === true) {
+        switchToDefaultMode(registration, registratedUserID);
       } else {
-        navigator.serviceWorker
-          .register("/register-service-worker", {
-            scope: "/", // global scope
-          })
-          .then((registration) => {
-            console.log("Service Worker registered:", registration);
-
-            handleNetworkModeSelector(registration);
-          })
-          .catch((error) => {
-            console.log(
-              "Fehler bei der Registrierung des Service Workers:",
-              error
-            );
-          });
-      }
-    });
-  } else {
-    document.getElementById("wifi-bar").style.display = "none"; //if there is no service worker dont show the bar @all
-  }
-
-  async function handleNetworkModeSelector(registration) {
-    const registratedUserID =
-      document.getElementById("userIdentification").value; //used for syncing data to the right account
-
-    const showWifiBar = document.getElementById("show-wifi-bar");     // wifi bar is initially toggled and only shown through user interaction
-    const wifiBar = document.getElementById("wifi-bar");
-
-
-    const screenWidth =     // assumption desktop pcs or big tablets always have a stable network connection (wifi) so dont show offline mode
-      window.innerWidth ||
-      document.documentElement.clientWidth ||
-      document.body.clientWidth;
-    if (screenWidth > 1024) {
-      showWifiBar.style.display = "none";
-    }
-
-    // wifi bar is animated in through css translate animation
-    showWifiBar.addEventListener("click", () => {
-      showWifiBar.style.display = "none";
-      wifiBar.style.transform = "translateY(0%)";
-    });
-
-    // switch modes and sync data
-    const onlineSVG = document.getElementById("online-svg");
-    const offlineSVG = document.getElementById("offline-svg");
-    const syncSVG = document.getElementById("sync-svg");
-
-
-    const isOnlineModeOn = localStorage.getItem("wifi");     // check inital status wheter offline mode is on or not
-
-    // wait for service worker activation
-    async function waitForServiceWorkerActivation() {
-      while (!registration.active) {
-        await new Promise((resolve) => setTimeout(resolve, 100));
+        //show modal that the device is not online
+        offlineModal.querySelector("input").value = "Keine Internetverbindung";
+        showModal();
+        setTimeout(hideModal, 3000);
       }
     }
+  });
+}
 
-    await waitForServiceWorkerActivation();
+const activateServer = () => {
+  const turnServerOnModal = document.getElementById("turnServerOnModal");
+  turnServerOnModal.style.display = "block";
 
-    //get online status first => if !online then set the wifi to offline automatically
-    // if online set the custom wifi
+  const turnServerOnButton = document.getElementById("turn_on_server");
+  const cancelButton = document.getElementById("cancel_server");
 
-    // if the localStorage value is offline mode then use it immediately => else: getOnlineStatus from sw
+  turnServerOnButton.addEventListener("click", () => {
+    localStorage.setItem("wifi", "true");
+    window.location.href = "/logout";
+  })
+
+  cancelButton.addEventListener("click", () => {
+    turnServerOnModal.style.display = "none";
+  })
+}
+
+const syncOfflineData = (registration, registratedUserID) => {
+  registration.active.postMessage({
+    command: "syncOfflineData",
+    registratedUser: registratedUserID,
+  });
+
+  syncSVG.style.display = "none"; //dont display the sync button anymore
+  onlineSVG.style.display = "block";
+}
+
+
+const handleNetworkModeSelector = async (registration) => {
+
+    const registratedUserID = document.getElementById("userIdentification").value; //used for syncing data to the right account
+
+    const showStatusBar = document.getElementById("show-wifi-bar");
+    const statusBar = document.getElementById("wifi-bar");
+
+    if (window.innerWidth > DESKTOP_MIN_WIDTH) {
+      showStatusBar.style.display = "none";
+    }
+
+    showStatusBar.addEventListener("click", () => handleStatusBarClick(showStatusBar, statusBar));
+
+    const defaultNetworkMode = JSON.parse(localStorage.getItem("wifi"));     // check inital status wheter offline mode is on or not
+
+    await navigator.serviceWorker.ready;
     
-    if (isOnlineModeOn === "true") {
+    if (defaultNetworkMode) {
       registration.active.postMessage({
         command: "getOnlineStatus",
       });
-    } else if (isOnlineModeOn === "false") {
-      registration.active.postMessage("switchToOfflineMode"); //switch to offline Mode just in case
-      showOfflineSVG();
+    } else {
+      switchToOfflineMode(registration);
     }
 
     navigator.serviceWorker.addEventListener("message", (event) => {
@@ -89,105 +144,36 @@ document.addEventListener("DOMContentLoaded", () => {
         // if the device is only check for localstorage value | if offline then always set it to offline
         if (onlineStatus) {
           
-          if (isOnlineModeOn === "false") {
-            registration.active.postMessage("switchToOfflineMode");
-
-            showOfflineSVG();
+          if (!defaultNetworkMode) {
+            switchToOfflineMode(registration);
           } else {
-            registration.active.postMessage({
-              command: "switchToDefaultMode",
-              registratedUser: registratedUserID,
-            });
-
-            showOnlineSVG();
+            switchToDefaultMode(registration, registratedUserID);
           }
         }
       }
     });
 
     // bei auswahl neune status an den service worker senden und im localstorage für initiales laden speichern
-    onlineSVG.addEventListener("click", () => {
-      registration.active.postMessage("switchToOfflineMode");
-      syncSVG.style.display = "none";
-      localStorage.setItem("wifi", false);
-      showOfflineSVG(); //switch view to offline - always display current status
-    });
+    onlineSVG.addEventListener("click", () => switchToOfflineMode(registration));
+    offlineSVG.addEventListener("click", () => tryToSwitchToDefaultMode(registration, registratedUserID));
 
-    // the user is not allowed to switch to online mode when the device itself is offline
-    offlineSVG.addEventListener("click", () => {
-      registration.active.postMessage({
-        command: "getOnlineStatus",
-      });
-
-      navigator.serviceWorker.addEventListener("message", (event) => {
-        const data = event.data;
-
-        if (data.type === "swOnlineStatus") {
-          if (data.onlineStatus === true) {
-            registration.active.postMessage({
-              command: "switchToDefaultMode",
-              registratedUser: registratedUserID,
-            });
-            localStorage.setItem("wifi", true);
-            showOnlineSVG();
-          } else {
-            //show modal that the device is not online
-
-            offlineModal.querySelector("input").value =
-              "Keine Internetverbindung";
-
-            showModal();
-            setTimeout(hideModal, 3000);
-          }
-        }
-      });
-    });
 
     const toggleServerButton = document.getElementById("toggle-server");
-    toggleServerButton.addEventListener("click", e => {
-      e.preventDefault();
-
-      const turnServerOnModal = document.getElementById("turnServerOnModal");
-      turnServerOnModal.style.display = "block";
-
-      const turnServerOnButton = document.getElementById("turn_on_server");
-      const cancelButton = document.getElementById("cancel_server");
-
-      turnServerOnButton.addEventListener("click", () => {
-        localStorage.setItem("wifi", "true");
-        window.location.href = "/logout";
-      })
-
-      cancelButton.addEventListener("click", () => {
-        turnServerOnModal.style.display = "none";
-      })
-
-    })
+    toggleServerButton.addEventListener("click", () => activateServer());
 
     //wenn auf den online modus gewechselt wird und der service worker feststellt dass es ungespeicherte daten aus dem offline mode gibt dann wird dieser button angezeigt: um zu synchroniseren:
     navigator.serviceWorker.addEventListener("message", (event) => {
       const data = event.data;
 
-      // if the sw decides that the data isnt synced and we are online show the showSyncButton
+      // if data is not synced show sync button
       if (data.type === "showSyncButton" && navigator.onLine) {
-        syncSVG.style.display = "block";
-        onlineSVG.style.display = "none";
+        showSyncButton();
       }
     });
 
-    syncSVG.addEventListener("click", () => {
-      registration.active.postMessage({
-        command: "syncOfflineData",
-        registratedUser: registratedUserID,
-      });
-
-      syncSVG.style.display = "none"; //dont display the sync button anymore
-      onlineSVG.style.display = "block";
-    });
+    syncSVG.addEventListener("click", () => syncOfflineData(registration, registratedUserID));
 
     //ack button - sets modal display property to none
-    const offlineModal = document.getElementById("offlineModal");
-
     const waitForSyncModal = document.getElementById("waitForSyncModal"); //to inform the user process has started
 
     if ("serviceWorker" in navigator) {
@@ -197,9 +183,7 @@ document.addEventListener("DOMContentLoaded", () => {
         // if the data was synced show modal for 3 seconds
         if (data.type === "offlineSync") {
           waitForSyncModal.style.display = "none";
-
           showModal();
-
           setTimeout(hideModal, 2500);
         }
       });
@@ -212,56 +196,52 @@ document.addEventListener("DOMContentLoaded", () => {
 
           waitForSyncModal.style.display = "none";
 
-          offlineModal.querySelector("input").value =
-            "Fehler beim Synchronisieren";
+          const offlineModal = document.getElementById("offlineModal");
+          offlineModal.querySelector("input").value = "Fehler beim Synchronisieren";
           showModal();
 
           setTimeout(hideModal, 4000);
 
-          syncSVG.style.display = "none";
-          onlineSVG.style.display = "block";
+          showOnlineSVG();
         }
       });
 
       navigator.serviceWorker.addEventListener("message", (event) => {
         const data = event.data;
 
-        if (data.type === "showWaitForSyncModal") {
-          waitForSyncModal.style.display = "block";
-        }
-      });
-    }
+          if (data.type === "showWaitForSyncModal") {
+            waitForSyncModal.style.display = "block";
+          }
+        });
+      }
 
     const closeModalButton = document.getElementById("close-modal-button");
-    closeModalButton.addEventListener("click", e => {
-      e.preventDefault();
-      waitForSyncModal.style.display = "none";
-      waitForSyncModal.classList.remove("active");
-/* 
-      function closeModal() {
-        modal.classList.remove('active');
-      } */
-    })
+    closeModalButton.addEventListener("click", () => handleCloseModalButton(waitForSyncModal));
+}
 
-    // Funktion zum Einblenden des Modals
-    function showModal() {
-      offlineModal.style.display = "block";
-    }
 
-    // Funktion zum Ausblenden des Modals
-    function hideModal() {
-      offlineModal.style.display = "none";
-      offlineModal.querySelector("input").value = "Offline Daten synchronsiert";
-    }
+document.addEventListener("DOMContentLoaded", () => {
+  // check whether there is a service worker already installed
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.getRegistration().then((registration) => {
+      if (registration && registration.active) {
+        handleNetworkModeSelector(registration);
+      } else {
+        navigator.serviceWorker
+          .register("/register-service-worker", {
+            scope: "/", 
+          })
+          .then((registration) => {
+            console.log("Service Worker registered:", registration);
 
-    function showOnlineSVG() {
-      onlineSVG.style.display = "block";
-      offlineSVG.style.display = "none";
-    }
-
-    function showOfflineSVG() {
-      onlineSVG.style.display = "none";
-      offlineSVG.style.display = "block";
-    }
+            handleNetworkModeSelector(registration);
+          })
+          .catch((error) => {
+            console.log("Fehler bei der Registrierung des Service Workers:", error);
+          });
+      }
+    });
+  } else {
+    document.getElementById("wifi-bar").style.display = "none"; //if there is no service worker dont show the bar @all
   }
 });
